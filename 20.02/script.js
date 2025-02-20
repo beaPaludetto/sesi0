92e5c4477403a7b1cab64b2f62ebdff8b668511f
beaPaludetto <beatrizpalubarreto@gmail.com>
2025-02-20 08:39:30 -0300

    adc variavel

--- 20.02/script.js
+++ 20.02/script.js
@@ -1,1 +1,7 @@
-console.log("ola")
+const nome = prompt("digite seu nome")
+const idade = prompt("digite sua idade")
+
+// console.log("meu nome é " + nome + " minha idade é " + idade + " anos")
+console.log(`meu nome é ${nome} e minha idade é ${idade} anos`)
+
+
