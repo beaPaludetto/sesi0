dbe67e19d20891513c76d393a88bf9f401e5d3cb
beaPaludetto <beatrizpalubarreto@gmail.com>
2025-02-20 08:44:23 -0300

    adc outras variaveis

---
+++ 20.02/variavel2.js
@@ -0,0 +1,8 @@
+let nome = "Ana Beatriz"; //string
+let idade = 16; // int
+let solteira = true; //booleano
+
+//imprimindo variaveis
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof solteira)
