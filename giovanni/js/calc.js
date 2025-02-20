f117b2b770307870baef95e1fed725dcb150b968
beaPaludetto <beatrizpalubarreto@gmail.com>
2025-02-20 08:07:43 -0300

    adicionando mais lições

---
+++ giovanni/js/calc.js
@@ -0,0 +1,17 @@
+//  declaração de variavel
+let num1 = prompt("digite um numero")
+let num2 = prompt("digite um outro numero")
+
+
+//operações matematicas
+let soma = num1 + num2
+let sub = num1 - num2
+let mult = num1 * num2
+let div = num1 / num2
+
+
+//  exibindo os resultados no console
+console.log("o resultado da soma é:" + soma)
+console.log("o resultado da sub é: " + sub)
+console.log("o resultado da mult é: " + mult)
+console.log("o resultado da div é: " + div)
